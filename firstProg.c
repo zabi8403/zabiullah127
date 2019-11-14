e0e49d79122132befdf148b14ca14e8a00912cbd
zabi8403 <56691283+zabi8403@users.noreply.github.com>
2019-11-14 15:25:41 +0500

    Add files via upload
    converstion for program

---
+++ firstProg.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main (void)
+{
+float data; float answer;
+int choice;
+printf("1. GB to MB \n");
+printf("2.MB to GB \n");
+printf("Enter a data: \n ");
+scanf("%f", &data);
+printf("Enter choice: ");
+scanf("%d", &choice);
+if(choice==1)
+{
+answer=1024*data;
+}
+else if(choice==2)
+{
+answer=data/1024;
+}
+printf("The result is %f", answer)
+}
